882de4406fa8c1fe60af854e041ff558e5d9da0a
kity599 <2193509409@qq.com>
2023-09-19 23:02:52 +0800

    API调用

--- chatbot-api-interfaces/src/test/java/cn/kity/chatbot/api/test/ApiTest.java
+++ chatbot-api-interfaces/src/test/java/cn/kity/chatbot/api/test/ApiTest.java
@@ -1,4 +1,30 @@
 package cn.kity.chatbot.api.test;
 
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+
+import java.io.IOException;
+
 public class ApiTest {
+    @Test
+    public void query_unanswered_questions() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        HttpGet get = new HttpGet("https://api.zsxq.com/v2/groups/28885518425541/topics?scope=all&count=20");
+
+        get.addHeader("cookie","sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%221894996a17b136a-0a8ac7e2389ba3-26031d51-1327104-1894996a17cf2f%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTg5NDk5NmExN2IxMzZhLTBhOGFjN2UyMzg5YmEzLTI2MDMxZDUxLTEzMjcxMDQtMTg5NDk5NmExN2NmMmYifQ%3D%3D%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%22%2C%22value%22%3A%22%22%7D%2C%22%24device_id%22%3A%221894996a17b136a-0a8ac7e2389ba3-26031d51-1327104-1894996a17cf2f%22%7D; abtest_env=product; zsxq_access_token=81EC04D2-2DEF-758B-84B8-74F240E06EE8_3DD14E1139112A11; zsxqsessionid=54994b055063af37e5ea11ecae6828db");
+        get.addHeader("Content-Type","application/json, text/plain, */*");
+
+        CloseableHttpResponse response = httpClient.execute(get);
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        }else {
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+    }
 }
